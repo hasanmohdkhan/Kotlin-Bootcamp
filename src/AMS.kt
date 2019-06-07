1e801729ca28d214a1c2c7893752856296d4e607
Hasanzian <hasanzian@gmail.com>
2019-06-07 14:45:48 +0530

    fun main(args: Array<String>) {

--- src/AMS.kt
+++ src/AMS.kt
@@ -1,25 +1,39 @@
-import java.util.*
-
-fun main() {
-    dayOfWeek()
-
-
+fun main(args: Array<String>) {
+    var fortune: String
+    for (i in 1..10) {
+        fortune = lesson_2.getFortune(lesson_2.getBirthday())
+        println("\nYour fortune is: $fortune")
+        if (fortune.contains("Take it easy")) break
+    }
 }
 
-fun dayOfWeek() {
-    println("What day is it today?")
-    val dayOfWeek = Calendar.getInstance().get(Calendar.DAY_OF_WEEK)
-    print(
-        when (dayOfWeek) {
-            1 -> "Sunday"
-            2 -> "Monday"
-            3 -> "Tuesday"
-            4 -> "Wednesday"
-            5 -> "Thursday"
-            6 -> "Friday"
-            7 -> "Saturday"
-            else -> "Time has stopped"
-        }
+fun getFortune(birthday: Int): String {
+
+    val fortune = listOf(
+        "You will have a great day!",
+        "Things will go well for you today.",
+        "Enjoy a wonderful day of success.",
+        "Be humble and all will turn out well.",
+        "Today is a good day for exercising restraint.",
+        "Take it easy and enjoy life!",
+        "Treasure your friends because they are your greatest fortune."
     )
 
+    val index = when(birthday){
+        in 28..31 -> 4
+        in 1..7 -> 2
+        else -> birthday.rem(fortune.size)
+    }
+
+
+    return  fortune[index]
+
 }
+
+fun getBirthday(): Int {
+    print("Enter birthday : ")
+    return readLine()?.toIntOrNull() ?:1
+}
+
+
+
--- src/HourOfDay.kt
+++
@@ -1,7 +0,0 @@
-fun main(){
-  val str = print("hello")
-    print(str)
-
-
-
-}
