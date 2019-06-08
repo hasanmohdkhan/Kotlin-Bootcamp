03d4e192241947bbcbda15ca7477c030b09fa2ae
Hasanzian <hasanzian@gmail.com>
2019-06-08 15:58:56 +0530

    Inheritance in Kotlin ------------------------ - classes with "open" modifier is inheritable - syntax to inheritance     class eBook(title :String):Book(title){} - for overriding declaration of function use "open"     1- open fun readPage(){....} // must have     2- override fun readPage(){...} // overriding here

---
+++ src/classes/Book.kt
@@ -0,0 +1,27 @@
+package classes
+
+/*
+1- Create a class, Book, with a title and an author.
+2- Add a method, readPage(), that increases the value of a private variable, currentPage, by 1.
+3- Create a subclass of Book; name it eBook.
+4- eBook also takes in a format, which defaults to "text".
+5- In eBooks, counting words makes more sense than pages.
+   Override the readPage() method to increase the word count by 250,
+   the average number of words per page from typewriter days.
+*/
+
+open class Book(val title: String, val author: String) {
+    private var currentPage = 1
+    open fun readPage() {
+        currentPage++
+    }
+
+
+}
+
+class eBook(title: String, author: String, var format: String = "text") : Book(title, author) {
+    private var readWords = 0
+    override fun readPage() {
+        readWords += 250
+    }
+}
